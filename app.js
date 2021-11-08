ab9eb11f795bd77d1640da858ae2adae08d876bb
Angélica Sofía Pagés Burgos <68571768+angelicapages@users.noreply.github.com>
2021-11-08 13:21:29 -0300

    podemos obtener el primer valor para poder realizar la operacion

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
-
 const botonesNumeros = document.querySelectorAll('.numero');
 const botonesOperadores = document.querySelectorAll('.operador');
 const resultado = document.getElementById("resultado")
+let numeroSelecionado = ""
+let primerElemento
 
-function agregarNumero(boton){
-    console.log(boton)
+function agregarNumero(boton) {
+    botonString = boton.toString()
+    numeroSelecionado = botonString + numeroSelecionado
+    console.log("numero seleccionado" + numeroSelecionado)
+    pasarStringANumero(numeroSelecionado)
 }
 
+function pasarStringANumero(string){
+numeroSelecionado= parseInt(string)
+console.log(numeroSelecionado)
+}
 botonesNumeros.forEach(boton => {
     boton.addEventListener('click', () => agregarNumero(boton.innerHTML));
 });
 
-// botonesOperadores.forEach(boton => {
-//     boton.addEventListener('click', () => computar(boton.value))
-// });
+botonesOperadores.forEach(boton => {
+    boton.addEventListener('click', () => computar(boton.value))
+});
 
 
 
